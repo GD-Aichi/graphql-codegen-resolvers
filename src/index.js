d474b66cb50bcd7f257d7d0563ffcd143647cd7a
Aichi <jens.kolberg@gamesdynamics.com>
2018-03-27 16:10:46 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const type = fs.readFileSync(__dirname + '/type.handlebars', 'utf8');
+const union = fs.readFileSync(__dirname + '/union.handlebars', 'utf8');
+
+const config = {
+  inputType: 'MULTIPLE_FILES',
+  templates: {
+    type,
+    union
+  },
+  flattenTypes: true,
+  filesExtension: 'ts',
+  customHelpers: {
+    hasTypes: (context, options) => {
+      if (context && context.fields && Array.isArray(context.fields)) {
+        const fields = context.fields;
+        const filter = fields.filter(field => {
+          return field.isType;
+        });
+
+        if (filter.length) {
+          return options && options.fn ? options.fn(context) : '';
+        }
+      }
+
+      return options && options.inverse ? options.inverse(context) : '';
+    }
+  }
+};
+
+module.exports = {
+  default: config
+};
